fd467ee73765226b82c2cb5845393a302e28c722
WIlliam Kesuma <127371994+WilliamKesuma@users.noreply.github.com>
2024-10-16 21:25:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Smooth scrolling for the "Explore Now" button
-document.querySelector('.btn-primary').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('.features-section').scrollIntoView({ behavior: 'smooth' });
-});
